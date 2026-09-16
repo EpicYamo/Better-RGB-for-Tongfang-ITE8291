55628bd8ae740f3b0eff0f4452bdcb1845d8264b
TheYamo <87617257+EpicYamo@users.noreply.github.com>
2026-09-16 23:14:54 +0300

    Wire resources: icon + manifest

--- src/main.c
+++ src/main.c
@@ -1,38 +1,39 @@
 #include <windows.h>
+#include "resource.h"
 
-static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
-    if (msg == WM_DESTROY) {
-        PostQuitMessage(0);
-        return 0;
-    }
-    return DefWindowProc(hwnd, msg, wp, lp);
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	if (msg == WM_DESTROY)
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wp, lp);
 }
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR cmd, int show) {
-    (void)hPrev;
-    (void)cmd;
-
-    WNDCLASS wc = {0};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInst;
-    wc.lpszClassName = L"BetterRGBWindowClass";
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    RegisterClass(&wc);
-
-    HWND hwnd = CreateWindow(
-        L"BetterRGBWindowClass", L"Better RGB (skeleton)",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
-        NULL, NULL, hInst, NULL
-    );
-
-    ShowWindow(hwnd, show);
-
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return 0;
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR cmd, int show)
+{
+	(void)hPrev;
+	(void)cmd;
+	WNDCLASS wc = {0};
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInst;
+	wc.lpszClassName = L"BetterRGBWindowClass";
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_APPICON));
+	RegisterClass(&wc);
+	HWND hwnd = CreateWindow(
+		L"BetterRGBWindowClass", L"Better RGB (skeleton)",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
+		NULL, NULL, hInst, NULL
+	);
+	ShowWindow(hwnd, show);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (0);
 }
